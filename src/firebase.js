850855b57051dd581c7c660ec6f6446bbe3506cc
Garv2022 <garv7183@gmail.com>
2024-04-06 23:48:26 +0530

    Home Page of the website

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCsSkg5A6spN2Pt3Z0Mrm1hrlcmnEA9tfQ",
+    authDomain: "shopomania-78ceb.firebaseapp.com",
+    projectId: "shopomania-78ceb",
+    storageBucket: "shopomania-78ceb.appspot.com",
+    messagingSenderId: "863903189870",
+    appId: "1:863903189870:web:b98aa74a6e6a4c12facf7f",
+    measurementId: "G-HGYCHQ3H7V"
+  };
